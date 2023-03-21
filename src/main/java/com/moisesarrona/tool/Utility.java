ffa17a300626d4a2c35e768c38c65c94c5c04788
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-03-20 23:30:36 -0600

    Add Utility class
    Add package tool with utility class

---
+++ src/main/java/com/moisesarrona/tool/Utility.java
@@ -0,0 +1,79 @@
+package com.moisesarrona.tool;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Utility implements UtilityI {
+
+    @Override
+    public String readFile(String path) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            StringBuilder sb = new StringBuilder();
+            String line = reader.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append(System.lineSeparator());
+                line = reader.readLine();
+            }
+
+            reader.close();
+            return sb.toString();
+        } catch (IOException e) {
+            throw new RuntimeException("Error reading file: ", e);
+        }
+    }
+
+    @Override
+    public List<String> getClassEdi(String packageName) {
+        String packagePath = packageName.replace(".", "/");
+        File packageDir = new File("src/main/java/" + packagePath);
+        File[] files = packageDir.listFiles();
+        List<String> ediCLass = new ArrayList<>();
+
+        assert files != null;
+        Arrays.stream(files).forEach((file) -> {
+            if (file.isFile() && file.getName().endsWith(".java")) {
+                String className = file.getName().substring(0, file.getName().length() - 5);
+                ediCLass.add(className);
+            }
+        });
+
+        return ediCLass;
+    }
+
+    @Override
+    public Object setFieldClassEdi(String packageName, String className) {
+        try {
+            Class<?> cls = Class.forName(packageName + className);
+            Constructor<?> constructor = cls.getDeclaredConstructor();
+            Object instance = constructor.newInstance();
+            Field[] fields = cls.getDeclaredFields();
+
+            Arrays.stream(fields).forEach((field) -> {
+                field.setAccessible(true);
+                try {
+                    field.set(instance, "P");
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException("Error setting values: ", e);
+                }
+            });
+
+            return instance;
+
+        } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException |
+                 InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException("Error reading class, declaring constructor or instancing constructor, : ", e);
+        }
+    }
+
+}
---
+++ src/main/java/com/moisesarrona/tool/UtilityI.java
@@ -0,0 +1,13 @@
+package com.moisesarrona.tool;
+
+import java.util.List;
+
+public interface UtilityI {
+
+    public String readFile(String path);
+
+    public List<String> getClassEdi(String packageName);
+
+    public Object setFieldClassEdi(String packageName, String className);
+
+}
