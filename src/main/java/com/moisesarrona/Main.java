bab89e301844859a6785a7ccf2e224b75205a5bb
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-03-28 21:54:50 -0600

    Call new methods

--- src/main/java/com/moisesarrona/Main.java
+++ src/main/java/com/moisesarrona/Main.java
@@ -3,6 +3,14 @@ package com.moisesarrona;
 import com.moisesarrona.controller.Test;
 import com.moisesarrona.controller.Converter;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Objects;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,8 +18,13 @@ public class Main {
         Test test = new Test();
 
         Converter converter = new Converter();
-        System.out.println(converter.jsonFileToObject(test.getPath()));
-        System.out.println(converter.jsonStringToObject(test.getJson()));
+
+        System.out.println("Json to Object");
+        System.out.println(converter.jsonFileToObject(test.getPathJson()));
+        System.out.println(converter.jsonStringToObject(test.getStrJson()));
+
+        System.out.println("Edi to Object");
+        System.out.println(converter.ediFileToObject(test.getPathEdi()));
 
     }
 
