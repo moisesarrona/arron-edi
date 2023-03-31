e5da345afbbf8b79b0bfebfba999144ad463f620
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-03-30 23:14:22 -0600

    Create Global.java

---
+++ src/main/java/com/moisesarrona/tool/Global.java
@@ -0,0 +1,12 @@
+package com.moisesarrona.tool;
+
+public enum Global {
+
+    PATH_BODY_EDI("./src/main/java/com/moisesarrona/segments");
+
+    public final String item;
+
+    Global(String item) {
+        this.item = item;
+    }
+}
