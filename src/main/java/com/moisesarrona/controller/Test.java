285b6b52fa4ccf7212b68fb32801587095d7d2fc
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-05-04 23:26:41 -0600

    Create Test.java
    Create test to test class and files

---
+++ src/main/java/com/moisesarrona/controller/Test.java
@@ -0,0 +1,17 @@
+package com.moisesarrona.controller;
+
+import com.moisesarrona.segments.body.*;
+import com.moisesarrona.segments.detail.*;
+import com.moisesarrona.segments.heading.*;
+import com.moisesarrona.segments.summary.*;
+import com.moisesarrona.tool.Utility;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class Test {
+
+
+}
