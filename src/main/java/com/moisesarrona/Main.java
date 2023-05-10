152b3038ec9608c7141b3d14d6b13606004ed0fc
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-05-09 23:25:46 -0600

    Add comments

--- src/main/java/com/moisesarrona/Main.java
+++ src/main/java/com/moisesarrona/Main.java
@@ -16,7 +16,14 @@ public class Main {
 
         System.out.println("Json to EDI");
         System.out.println(converter.jsonFileToEdi(test.getPathJson()));
+        //System.out.println(converter.jsonStringToEdi(test.getStrEdi()));
 
+        //System.out.println("Object to Json");
+        //System.out.println(converter.objectToJson(test.getObjectEdi()));
+
+
+        //System.out.println("Edi to Object");
+        //System.out.println(converter.ediFileToObject(test.getPathEdi()));
 
     }
 
