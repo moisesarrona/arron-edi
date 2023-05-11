8d56e8d4be269346644e3faf2a1b346ae3f7a403
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-05-10 22:36:57 -0600

    Converter json to object

--- src/main/java/com/moisesarrona/Main.java
+++ src/main/java/com/moisesarrona/Main.java
@@ -1,8 +1,18 @@
 package com.moisesarrona;
 
+import com.moisesarrona.controller.Converter;
+import com.moisesarrona.controller.Test;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Test test = new Test();
+        Converter converter = new Converter();
+
+        System.out.println("Json to Object");
+        System.out.println(converter.jsonFileToObject(test.getPathJson()));
+        System.out.println(converter.jsonStringToObject(test.getStrJson()));
 
     }
 
