807caf7f11d11ba1ed2d836f0730393e41982aa6
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-05-18 22:00:56 -0600

    Create Global
    Create Global class

--- src/main/java/com/moisesarrona/tool/Global.java
+++ src/main/java/com/moisesarrona/tool/Global.java
@@ -1,5 +1,15 @@
 package com.moisesarrona.tool;
 
-public enum Global {
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class Global {
+    public static final String PATH_HEAD = "com.moisesarrona.segments.body.Head";
+    public static final String PATH_DETAIL = "com.moisesarrona.segments.body.Detail";
+    public static final String PATH_SUMMARY = "com.moisesarrona.segments.body.Summary";
+    public static final String PATH_BODY_EDI = "./src/main/java/com/moisesarrona/segments";
+
+    public static Path getPackagePath() {
+        return Paths.get("src", "main", "java", "com", "moisesarrona", "segments");
+    }
 }
