b6a927b804c7bbd21f645074ef3cfcb57aa142fb
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-05-18 22:01:32 -0600

    Create jsonFileToEdi()
    Create jsonFileToEdi to converter json to edi

--- src/main/java/com/moisesarrona/controller/Converter.java
+++ src/main/java/com/moisesarrona/controller/Converter.java
@@ -2,10 +2,16 @@ package com.moisesarrona.controller;
 
 import com.google.gson.Gson;
 import com.moisesarrona.segments.body.Body;
+import com.moisesarrona.tool.Utility;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
+
+import com.moisesarrona.tool.Global;
 
 public class Converter {
 
@@ -23,6 +29,38 @@ public class Converter {
         if (json.isEmpty())
             throw new NullPointerException("Value is equals to null");
         return gson.fromJson(json, Body.class);
+    }
+
+    public String jsonFileToEdi(String json, Boolean path) {
+        Utility utility = new Utility();
+
+        String jsonContent = path ? utility.readFile(json) : json;
+        Path location = Global.getPackagePath();
+        Set<String> packages = utility.getPackages(location);
+
+        AtomicReference<String> jsonContentRef = new AtomicReference<>(jsonContent);
+        packages.forEach(packag -> {
+            List<String> clss = utility.getClassEdi(packag, true);
+            clss.forEach(cls -> {
+                List<String> fields = utility.getFieldClass(cls);
+                fields.forEach(field -> {
+                    if (!(cls.equals(Global.PATH_HEAD) || cls.equals(Global.PATH_DETAIL) ||
+                            cls.equals(Global.PATH_SUMMARY))) {
+                        jsonContentRef.set(jsonContentRef.get().replace(field, ""));
+                    }
+                });
+            });
+        });
+
+        return jsonContentRef.get()
+                .replaceAll(",", "").replaceAll("\\{", "")
+                .replaceAll("}", "~").replaceAll(":", "*")
+                .replaceAll("\\[", "").replaceAll("]", "")
+                .replaceAll("\"(.*?)\"|\\s+", "$1")
+                //repeated value replacement
+                .replaceAll("~{2,}", "~").replaceAll("\\*{2,}", "*")
+                .replaceFirst("\\*", "");
+
     }
 
 }
