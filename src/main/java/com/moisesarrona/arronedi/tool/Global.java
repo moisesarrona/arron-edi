18c13448a884e027cee75e264a372ddc56edd4a3
Moises Arrona <57106162+moisesarrona@users.noreply.github.com>
2023-08-17 22:22:56 -0600

    renaming constant paths

--- src/main/java/com/moisesarrona/arronedi/tool/Global.java
+++ src/main/java/com/moisesarrona/arronedi/tool/Global.java
@@ -8,12 +8,12 @@ import java.nio.file.Paths;
  * @version 0.1
  */
 public class Global {
-    public static final String PATH_HEAD = "com.moisesarrona.segments.body.Head";
-    public static final String PATH_DETAIL = "com.moisesarrona.segments.body.Detail";
-    public static final String PATH_SUMMARY = "com.moisesarrona.segments.body.Summary";
-    public static final String PATH_BODY_EDI = "./src/main/java/com/moisesarrona/segments";
+    public static final String PATH_HEAD = "com.moisesarrona.arronedi.segments.body.Head";
+    public static final String PATH_DETAIL = "com.moisesarrona.arronedi.segments.body.Detail";
+    public static final String PATH_SUMMARY = "com.moisesarrona.arronedi.segments.body.Summary";
+    public static final String PATH_BODY_EDI = "./src/main/java/com/moisesarrona/arronedi/segments";
 
     public static Path getPackagePath() {
-        return Paths.get("src", "main", "java", "com", "moisesarrona", "segments");
+        return Paths.get("src", "main", "java", "com", "moisesarrona", "arronedi", "segments");
     }
 }
